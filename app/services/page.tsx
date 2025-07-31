4107645ee11c53d44a5938234efdaa9e35dd8784
Simarpreet-Ditinus <168518406+Simarpreet-Ditinus@users.noreply.github.com>
2025-07-31 16:54:31 +0530

    Update
    Changed all URL's for services

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -86,7 +86,7 @@ export default function Services() {
 
                 <CardFooter className="p-0 mt-[-20]">
                   <Link
-                    href={"/services/" + service?.slug}
+                    href={"/" + service?.slug}
                     className="bg-[#0D378D] text-white pl-5 pr-2 py-1.5   border-2 border-white rounded-full font-medium  flex items-center justify-center"
                   >
                     {service.buttonText}
